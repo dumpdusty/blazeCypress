5db05eb4e07f7967bba3167bb5b64c48a36cfab3
dumpdusty <dumpdusty@gmail.com>
2022-11-10 10:14:47 -0800

    upd about-us.cy.js

--- cypress/e2e/about-us.cy.js
+++ cypress/e2e/about-us.cy.js
@@ -1,8 +1,22 @@
 describe('about us', () => {
+
     it('modal window opening', () => {
         cy.visit(`/`)
         cy.contains(`[data-toggle="modal"]`, 'About us').click()
         cy.get(`[class="btn btn-secondary"]`).should(`be.visible`)
+    });
 
+    it('video playing', () => {
+        cy.get(`video`).should(`have.prop`, `paused`, true)
+            .and(`have.prop`, `ended`, false)
+            .then(($video)=>{
+                $video[0].play()
+        })
+        cy.wait(10000)
+        cy.get(`video`).should(`have.prop`, `paused`, false)
+            .then(($video)=>{
+            $video[0].pause()
+        })
+        cy.get(`video`).should(`have.prop`, `paused`, true)
     });
 });
