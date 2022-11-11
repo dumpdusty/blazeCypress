d8ad6408fa1e5d6e0a1637e365e1128b664ce814
dumpdusty <dumpdusty@gmail.com>
2022-11-10 20:49:37 -0800

    upd item-page.cy.js

--- cypress/e2e/item-page.cy.js
+++ cypress/e2e/item-page.cy.js
@@ -3,13 +3,13 @@ describe('inspect item page', () => {
         cy.visit(`/`)
         cy.get(`.hrefch`).first().click()
 
-        cy.get(`#tbodyid`).each(($el) => {
-            cy.wrap($el).find('h2').should('contain', 'Samsung galaxy s6')
-            cy.wrap($el).find(`#more-information`).should('contain', 'Product description')
+        cy.get(`#tbodyid`).then((el) => {
+            cy.wrap(el).find('h2').should('contain', 'Samsung galaxy s6')
+            cy.wrap(el).find(`#more-information`).should('contain', 'Product description')
             cy.get('#more-information > p').should('be.visible')
-            cy.wrap($el).find(`.price-container`).invoke(`text`).should(`match`, /^\$\d*/)
+            cy.wrap(el).find(`.price-container`).invoke(`text`).should(`match`, /^\$\d*/)
                 .and('contain', ' *includes tax')
-            cy.wrap($el).find(`.col-sm-12 > .btn`).should('contain', 'Add to cart')
+            cy.wrap(el).find(`.col-sm-12 > .btn`).should('contain', 'Add to cart')
         })
     });
 })
